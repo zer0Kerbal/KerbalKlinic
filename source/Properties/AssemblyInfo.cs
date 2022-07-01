3a7ff060d39f2cbc9334a4e144157f22b40eaeee
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-07-01 15:52:48 -0500

    Create AssemblyInfo.cs

---
+++ source/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿#region License Kerbal Klinic (NRKK)
+/*
+ * An addon that introduces a method to resurrect dead kerbals for Kerbal Space Program by Squad.
+ *
+ * Copyright (C) 2018 NepalRAWR
+ * Copyright (C) 2019, 2022 zer0Kerbal (zer0Kerbal at hotmail dot com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */ 
+#endregion
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kerbal Klinic (NRKK")]
+[assembly: AssemblyDescription("An addon that introduces a method to resurrect dead kerbals for Kerbal Space Program.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kerbal Klinic (NRKK)")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 NepalRAWR; (c) 2021,2022 zer0Kerbal")]
+[assembly: AssemblyTrademark("Trademark (tm) 2018 NepalRAWR; (tm) 2021,2022 zer0Kerbal")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a501eaad-abea-43ed-83f6-b03c39b04a5b")]
