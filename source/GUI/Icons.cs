06d7f4c753f0ceaf3edcecd0e2632651f7255ae6
Lisias T <me@lisias.net>
2022-07-14 23:24:13 -0300

    Implementing a minimal Stock Toolbar suppport.

---
+++ source/GUI/Icons.cs
@@ -0,0 +1,50 @@
+﻿/*
+	This file is
+		© 2022 LisiasT
+
+	And licensed to zer0Kerbal under:
+		* GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
+
+	You should have received a copy of the GNU General Public License 3.0
+	along with this product. If not, see <https://www.gnu.org/licenses/>.
+*/
+using System;
+using System.IO;
+
+using UnityEngine;
+
+using RUI.Icons.Selectable;
+
+namespace KerbalKlinic.UI
+{
+	internal static class Icons
+	{
+		private const string ICONSDIR = "Icons";
+
+		private static Texture2D _Button = null;
+		internal static Texture2D Button = _Button ?? (_Button = LoadFromFile("button"));
+
+		private static Texture2D _ButtonOff = null;
+		internal static Texture2D ButtonOff = _ButtonOff ?? (_ButtonOff = LoadFromFile("button_off"));
+
+		private static Texture2D _ButtonOn = null;
+		internal static Texture2D ButtonOn = _ButtonOn ?? (_ButtonOn = LoadFromFile("button_on"));
+
+		private static Texture2D LoadFromFile(string iconFile)
+		{
+			string root = Path.GetDirectoryName(typeof(Icons).Assembly.Location);
+			string pathname = Path.Combine(root,"PluginData", ICONSDIR, iconFile+".png");
+			Texture2D t = new(2, 2, TextureFormat.RGBA32, false);
+			ImageConversion.LoadImage(t, File.ReadAllBytes(pathname));
+			return t;
+		}
+
+		private static Icon _Icon = null;
+		internal static Icon Icon = _Icon ?? (_Icon = GenIcon());
+		private static Icon GenIcon()
+		{
+			Icon icon = new Icon("ButtonIcon", UI.Icons.ButtonOff, UI.Icons.ButtonOn);
+			return icon;
+		}
+	}
+}
---
+++ source/GUI/ToolbarSupport.cs
@@ -0,0 +1,87 @@
+﻿/*
+	This file is
+		© 2022 LisiasT
+
+	And licensed to zer0Kerbal under:
+		* GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
+
+	You should have received a copy of the GNU General Public License 3.0
+	along with this product. If not, see <https://www.gnu.org/licenses/>.
+*/
+using UnityEngine;
+
+using KSP.UI.Screens;
+
+namespace KerbalKlinic.UI
+{
+	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	internal class ToolbarSupport : MonoBehaviour
+	{
+		private static ToolbarSupport instance = null;
+		internal static ToolbarSupport Instance => instance;
+		private ApplicationLauncherButton button;
+
+		private void Awake() {
+			DontDestroyOnLoad(this);
+			instance = this;
+		}
+
+		private void Start() {
+			GameEvents.onGUIApplicationLauncherReady.Add(this.OnGUIApplicationLauncherReady);
+			GameEvents.onGUIApplicationLauncherDestroyed.Add(this.OnGUIApplicationLauncherDestroyed);
+		}
+
+		private void OnDestroy() {
+			instance = null;
+			this.OnGUIApplicationLauncherDestroyed();	// Force the destruction of the Current Button
+			GameEvents.onGUIApplicationLauncherDestroyed.Remove(this.OnGUIApplicationLauncherDestroyed);
+			GameEvents.onGUIApplicationLauncherReady.Remove(OnGUIApplicationLauncherReady);
+		}
+
+		private void OnGUIApplicationLauncherReady() {
+			if (null == this.button)
+				this.button = ApplicationLauncher.Instance.AddModApplication(
+							this.OnTrue, this.OnFalse
+							, this.Dummy, this.Dummy
+							, this.Dummy, this.Dummy
+							, ApplicationLauncher.AppScenes.SPACECENTER
+							, UI.Icons.Button
+						)
+					;
+			this.UpdateIcon();
+		}
+
+		internal void UpdateIcon() {
+			if (null == KerbalKlinic.Instance) this.button.SetTexture(UI.Icons.Button);
+			else this.button.SetTexture(KerbalKlinic.Instance.ShowMenu ? UI.Icons.ButtonOn : UI.Icons.ButtonOff);
+		}
+
+		internal void UpdateIcon(bool active, bool available) {
+			if (null != KerbalKlinic.Instance && active && available) this.UpdateIcon();
+			else this.button.SetTexture(UI.Icons.Button);
+		}
+
+		private void OnGUIApplicationLauncherDestroyed() {
+			if (null == this.button) return;
+			ApplicationLauncher.Instance.RemoveModApplication(this.button);
+			Destroy(this.button);
+			this.button = null;
+		}
+
+		private void Dummy() {
+			// Dummy Bears!!! :P
+		}
+
+		private void OnTrue() {
+			if (null != KerbalKlinic.Instance)		// Better safe than sorry.
+				KerbalKlinic.Instance.ShowMenu = true;
+			this.UpdateIcon();
+		}
+
+		private void OnFalse() {
+			if (null != KerbalKlinic.Instance)		// Better safe than sorry.
+				KerbalKlinic.Instance.ShowMenu = false;
+			this.UpdateIcon();
+		}
+	}
+}
